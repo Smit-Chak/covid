3c28b65360526073eb641be857b1d1c0cb9267e8
Smit-Chak <smitangshuchakraborty@gmail.com>
2021-08-09 16:43:34 +0530

    Mode Changes
    Day and Night added

--- Html/JS/index.js
+++ Html/JS/index.js
@@ -1,8 +1,10 @@
-function mode() {
+function n_mode() {
+
     if (document.getElementById("mode").click) {
 
         let sheet = "./stylesheets/darkstyle.css";
         document.getElementById("pagestyle").setAttribute("href", sheet);
+        console.log("Test");
         /* Background color + Text Color 
         document.getElementById("mode").style.background = "#1F1B24";
         document.getElementById("mode_3").style.background = "#1F1B24";
@@ -11,5 +13,13 @@ function mode() {
         document.getElementById("mode_2").style.color = "white";
         */
         /* Hover Color */
+    }
+}
+
+function d_mode() {
+    if (document.getElementById("mode").click) {
+        let sheet = "./stylesheets/styles.css";
+        document.getElementById("pagestyle").setAttribute("href", sheet);
+        console.log("Test");
     }
 }
