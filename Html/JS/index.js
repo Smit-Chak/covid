91bc0fc8a6cf831856208632c6705b2217071f5b
Smit-Chak <smitangshuchakraborty@gmail.com>
2021-08-09 15:57:47 +0530

    Dark Mode Overhaul
    New Styleshseet and JS changed

--- Html/JS/index.js
+++ Html/JS/index.js
@@ -1,7 +1,15 @@
-function f() {
-    console.log("Test");
-}
+function mode() {
+    if (document.getElementById("mode").click) {
 
-function leave() {
-    console.log("Left");
+        let sheet = "./stylesheets/darkstyle.css";
+        document.getElementById("pagestyle").setAttribute("href", sheet);
+        /* Background color + Text Color 
+        document.getElementById("mode").style.background = "#1F1B24";
+        document.getElementById("mode_3").style.background = "#1F1B24";
+        document.getElementById("mode_3").style.color = "white";
+        document.getElementById("mode_2").style.background = "#121212";
+        document.getElementById("mode_2").style.color = "white";
+        */
+        /* Hover Color */
+    }
 }
